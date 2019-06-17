52d74aaea4623433f51c4524f1145f5046af18a2
dcxSt <stephen.fay@mail.mcgill.ca>
2019-06-17 11:11:49 +0200

    homogenise typos

--- db_homogenise_comments.py
+++ db_homogenise_comments.py
@@ -22,7 +22,7 @@ for all numbers in howevery big the database is:
             comment = ...
             if comment in one of the things"""
 
-# checked or unchecked
+# checked
 def homogenise_uncertain():
     ## n is the supremem of the set of ids
     #n = 206773
@@ -35,7 +35,6 @@ def homogenise_uncertain():
     data = db_search.select_all_data()
     # connect to the database
     cursor,mydb = db_connection.database_connector()
-    print(data[9][8])
     for i in data:
         id_number=i[0]
         comment=i[8]
@@ -46,4 +45,33 @@ def homogenise_uncertain():
     db_connection.close_database_connection(mydb=mydb)
 
 
-#homogenise_uncertain()
+def homogenise_typos():
+    S_et_W=["S et W","S et W "]
+    schwabe=["Schwabe","schwabe"]
+    wolf=["Wolf","wolf","wolfw"]
+    leppig=["Leppig","leppig"]
+    zucconi=["Zucconi","zucconi","ZUCCONI"]
+    change_of_instrument=["change of instrument","Change of instrument"]
+    mascari=["Mascari","mascari"]
+    schwarzenbrunner=["*=Schwarzenbrunner","*=schwarzenbrunner"]
+    waldmeier=["Waldmeier",
+    "waldmeier",
+    "W Analyse faite par Waldmeier",
+    "W Etude faite par Waldmeier",
+    "W Etude faite pas Waldmeier",
+    "? Etude faite pas Waldmeier",
+    "W = Etude faite pas Waldmeier",
+    "W = Etude faite par Waldmeier",]
+    to_change=[S_et_W,schwabe,wolf,leppig,zucconi,change_of_instrument,
+    mascari,schwarzenbrunner,waldmeier]
+    
+    data = db_search.select_all_data()
+    cursor,mydb=db_connection.database_connector()
+    for i in data:
+        id_number=i[0]
+        comment=i[8]
+        for j in to_change:
+            if comment in j:
+                db_edit.set_comment(id_number,j[0],cursor=cursor,mydb=mydb,replace=True)
+    db_connection.close_database_connection(mydb=mydb)
+
